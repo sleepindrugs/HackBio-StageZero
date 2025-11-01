2dd0c3fa65205f9687de3eb29ec4d440daf68a81
sleepindrugs <tanishkakumarofficial@gmail.com>
2025-11-01 15:01:19 +0000

    Update Team_code.py

--- Team_code.py
+++ Team_code.py
@@ -3,37 +3,34 @@
 team = [
     {
         "name": "Sweeta Selvan",
-        "slack_username": "@sweeta",
+        "slack_username": "@sweeta selvan",
         "country": "India",
         "hobby": "Watching films",
         "affiliation": "Student (Anna University, Chennai), UAA",
         "favorite_gene": {
             "gene_name": "TP53",
             "dna_sequence": "ATGGAGGAGCCGCAGTCAGATCCTAGCGTCGAGCCCCCTCTGAGTCAGGAAACATTTTCAGACCTATGGAAACTACTTCCTGAAAACA"
-        }
-    },
+        }   },
     {
         "name": "Aditya Dash",
-        "slack_username": "@aditya",
+        "slack_username": "@aditya dash",
         "country": "India",
         "hobby": "Playing cricket",
         "affiliation": "Student (IIT Bhubaneswar)",
         "favorite_gene": {
             "gene_name": "BRCA1",
             "dna_sequence": "ATGGAAGTTGTCATTTTATAAAAAGGTTGCAAAATAATGTGGAAGAAAAAAAGCAGTGAACCTGTTGGTGATTTTGAAATGAGGAGGA"
-        }
-    },
+        }  },
     {
         "name": "Gloria Omogwigho Akpederi",
-        "slack_username": "@gloria",
+        "slack_username": "@gloria ",
         "country": "Nigeria",
         "hobby": "Baking",
         "affiliation": "Student (University of Lagos)",
         "favorite_gene": {
             "gene_name": "ACE2",
             "dna_sequence": "ATGGAGAGCCCTGTTCACATCTCCTCCTGCTCTCCTGCTCCTGCTCCTGCTCCTGGAGGAGGAGATCTTCCTCCTGCTGCTCTGCCT"
-        }
-    },
+        }  },
     {
         "name": "Umunakwe Goodness",
         "slack_username": "@goodness",
@@ -43,8 +40,7 @@ team = [
         "favorite_gene": {
             "gene_name": "HBA1",
             "dna_sequence": "ATGGTGCACCTGACTCCTGAGGAGAAGTCTGCCGTTACTGCCCTGTGGGGCAAGGTGAACGTGGATGAAGTTGGTGGTGAGGCCCTGG"
-        }
-    }
+        } }
 ]
 
 # Print the information
@@ -56,4 +52,4 @@ for member in team:
     print(f"Affiliation: {member['affiliation']}")
     print(f"Favorite Gene: {member['favorite_gene']['gene_name']}")
     print(f"DNA Sequence: {member['favorite_gene']['dna_sequence']}")
-    print("-" * 70)
+    print("-" * 20)
